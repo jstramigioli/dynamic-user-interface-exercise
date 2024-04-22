d476ba6efe93cb0dfad69866174e27580e24f808
jstramigioli <julianstramigioli@gmail.com>
2024-04-21 22:39:22 -0300

    Added ability to create dropdown menu from existing html elements

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@
 
 const dropDownMenu = (title, parent, displayMode) => {
     
-    const dropDownMenu = document.createElement('div')
+    const dropDownMenu = parent
     dropDownMenu.classList.add('dropdown-menu')
-    parent.appendChild(dropDownMenu)
 
     const menuHeader = document.createElement('button')
     menuHeader.classList.add('dropdown-header')
@@ -84,6 +83,77 @@ const dropDownMenu = (title, parent, displayMode) => {
 
 }
 
+const dropDownMenuFromHtml = (menuElement, displayMode) => {
+    
+    const dropDownMenu = menuElement
+    dropDownMenu.classList.add('dropdown-menu')
+
+    const menuHeader = dropDownMenu.children[0]
+    console.log(menuHeader)
+    menuHeader.classList.add('dropdown-header')
+
+    const elementsContainer = dropDownMenu.children[1]
+
+    const elements = elementsContainer.children
+
+    switch (displayMode) {
+        case 'click':
+            menuHeader.addEventListener('click', (e) => {
+                for ( let i = 0 ; i < elements.length ; i++) {
+                    toggleVisible(elements[i])
+                    console.log(e.target)
+                }
+            elementsContainer.addEventListener('click', (e) => {
+                e.stopPropagation()
+            })
+            });
+            break;
+        case 'hover':
+            dropDownMenu.addEventListener('mouseover', () => {
+                for ( let i = 0 ; i < elements.length ; i++) {
+                    addVisible(elements[i])
+                }
+            })
+            dropDownMenu.addEventListener('mouseout', () => {
+                for ( let i = 0 ; i < elements.length ; i++) {
+                    removeVisible(elements[i])
+                }
+            })
+            break;
+        default:
+            dropDownMenu.addEventListener('mouseover', () => {
+                for ( let i = 0 ; i < elements.length ; i++) {
+                    addVisible(elements[i])
+                }
+            })
+            dropDownMenu.addEventListener('mouseout', () => {
+                for ( let i = 0 ; i < elements.length ; i++) {
+                    removeVisible(elements[i])
+                }
+            })
+            break;
+    }
+  
+    function addVisible(el) {
+        el.classList.add('visible')
+    }
+    
+    function removeVisible(el) {
+        el.classList.remove('visible')
+    }
+
+    function toggleVisible(el) {
+        el.classList.toggle('visible')
+    }
+
+
+    return {
+        newElement,
+        removeElement
+    }
+
+}
+
 import './style.css';
 
 const menuClickContainer = document.querySelector('#menu-click')
@@ -97,4 +167,7 @@ const menuHover = dropDownMenu('Hover Menu', menuHoverContainer, 'hover')
 menuHover.newElement('Element 1')
 menuHover.newElement('Element 2')
 menuHover.newElement('Element 3')
+
+const menuFromHtml = document.querySelector('#menu-from-html')
+dropDownMenuFromHtml(menuFromHtml, 'hover')
 
